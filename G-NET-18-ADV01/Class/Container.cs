337ed8e23b0b8b0dee1fb93f4ab040a52732a2ef
joeali31 <ya9415565@gmail.com>
2026-03-26 14:58:43 -0700

    Question02

---
+++ G-NET-18-ADV01/Class/Container.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_18_ADV01.Class
+{
+    public class Container<T>
+    {
+        private T _item;
+
+        public Container(T item)
+        {
+            _item = item;
+        }
+
+        public void Add(T item)
+        {
+            _item = item;
+        }
+
+        public T Get()
+        {
+            return _item;
+        }
+    }
+}
--- G-NET-18-ADV01/Program.cs
+++ G-NET-18-ADV01/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace G_NET_18_ADV01
+﻿using G_NET_18_ADV01.Class;
+using System.ComponentModel;
+
+namespace G_NET_18_ADV01
 {
     internal class Program
     {
@@ -10,6 +13,12 @@
              * What is a generic : A generic class is a class that can work with any data type using a type parameter
              * Why use generics : No boxing/unboxing , Type safety , Reusability
              */
+            #endregion
+
+
+            #region Question02
+            /*Write a generic class Container<T> with Add and Get methods.*/
+            Container<int> number = new(10);
             #endregion
 
         }
