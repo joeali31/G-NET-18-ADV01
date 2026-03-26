cbccb8f67f2407fbc38fa4cc814ac56e62b0a0ed
joeali31 <ya9415565@gmail.com>
2026-03-26 15:21:43 -0700

    Question11

---
+++ G-NET-18-ADV01/Class/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_18_ADV01.Class
+{
+    public class Animal
+    {
+        public void Speak() { }
+    }
+
+    public class ContainerAnimal<T> where T : Animal 
+    {
+        public T Item { get; set; }
+
+        public void CallSpeak()
+        {
+            Item.Speak();
+        }
+    }
+}
--- G-NET-18-ADV01/Program.cs
+++ G-NET-18-ADV01/Program.cs
@@ -79,6 +79,13 @@ namespace G_NET_18_ADV01
             #endregion
 
 
+            #region Question11
+            /*Q11: What is the base class constraint? Write an example.
+             * Restricts T to inherit from a specific base class.
+             */
+            #endregion
+
+
         }
 
         public static void Swap<T>(ref T a, ref T b)
