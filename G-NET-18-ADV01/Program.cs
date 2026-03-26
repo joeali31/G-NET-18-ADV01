2fe5ae7ccc317fea294bd5591ee9006cac54ab65
joeali31 <ya9415565@gmail.com>
2026-03-26 14:53:39 -0700

    Question01

--- G-NET-18-ADV01/Program.cs
+++ G-NET-18-ADV01/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+
+            #region Question01
+            /*Q1: What is a generic class? Why use generics?
+             * What is a generic : A generic class is a class that can work with any data type using a type parameter
+             * Why use generics : No boxing/unboxing , Type safety , Reusability
+             */
+            #endregion
+
         }
     }
 }
